2e8e167a040c1bddbfd02b46e886fa29287734f8
Carlos Tirado Cortes <11283924@ad.feit.uts.edu.au>
2017-12-06 20:20:17 +1100

    Trying to fix an error with the dots of the spline.

--- Assets/Scripts/BezierCurve.cs
+++ Assets/Scripts/BezierCurve.cs
@@ -24,7 +24,8 @@ public class BezierCurve : MonoBehaviour {
 
     public Vector3 GetVelocity ( float t)
     {
-        return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) - transform.position;
+        return transform.TransformPoint(
+            Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) - transform.position;
     }
 
     public Vector3 GetDirection(float t)
